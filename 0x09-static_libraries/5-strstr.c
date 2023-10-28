0b0170f7cfbbabf125761c6021baebaee7738a43
Lynnagneta <lynnetteagneta@live.com>
2023-10-28 02:07:52 -0700

    0x09-static_libraries

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr -  a function that locates a substring.
+ * @haystack: an input string to search in
+ * @needle: an input string to locate into string haystack
+ * Return: , (\0) success always
+ * or NULL if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *one = haystack;
+		char *two = needle;
+
+		while (*one == *two && *two != '\0')
+		{
+			one++;
+			two++;
+		}
+
+		if (*two == '\0')
+			return (haystack);
+	}
+
+	return (NULL);
+}
